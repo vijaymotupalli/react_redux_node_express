e47cee090fc137ecdc41eb02ff00460b6d9986d0
Admin <Admin>
2017-07-02 15:13:24 +0530

    added client folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackconfig from '../webpack.config.dev';
+
 let app = express()
+
+app.use(webpackMiddleware(webpack(webpackconfig)));
 
 app.get('/*' ,(req,res)=>{
 
-    res.sendfile(path.join(__dirname,'./index.html'));
+    res.sendFile(path.join(__dirname,'./index.html'));
 })
 
 app.listen(3000,()=>console.log("server running at 3000"));
